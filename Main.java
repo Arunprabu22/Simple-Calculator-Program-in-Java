5952817c91f51a0b319b83c73896f78ede2b74ba
Arun Prabu <vsb.arun.22cs3020@gmail.com>
2025-01-03 15:25:11 +0530

    Updated Calculator Program

--- Main.java
+++ Main.java
@@ -4,16 +4,20 @@ public class Main{
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     System.out.println("Simple Calculator");
+
     System.out.println("Choose Your Operator");
     System.out.println("1.Addition \n2.Subtraction\n3.Multiplication\n4.Division");
+
     System.out.println("Enter Your Choice");
     int Choice= scanner.nextInt();
+
     System.out.println("Enter First Number");
-    int First_Number = scanner.nextInt();
+    float First_Number = scanner.nextInt();
+
     System.out.println("Enter Second Number");
-    int Second_Number = scanner.nextInt();
+    float Second_Number = scanner.nextInt();
     
-    int Result=0;
+    float Result=0;
     switch(Choice){
       case 1:
           Result=First_Number+Second_Number;
